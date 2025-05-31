47448ee3de404e0f49bf70c870e3511346e7eceb
[guillermo quintanilla rodrigo] <[gquirod@uoc.edu]>
2025-06-01 01:25:09 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -49,8 +49,7 @@ import { NgOptimizedImage } from '@angular/common';
       enabled: !isDevMode(),
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000',
-      scope:'/pec_5/'
+      registrationStrategy: 'registerWhenStable:30000'
     })
   ],
   providers: [
